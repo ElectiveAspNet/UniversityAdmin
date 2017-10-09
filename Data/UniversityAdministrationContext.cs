878e30f312eaf49a8c8cab14a0552076905383ed
Claus Bove <clbo@kea.dk>
2017-10-09 10:04:38 +0200

    pause

---
+++ Data/UniversityAdministrationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UniversityAdministration.Models.Entities;
+
+namespace UniversityAdministration.Models
+{
+    public class UniversityAdministrationContext : DbContext
+    {
+        public UniversityAdministrationContext (DbContextOptions<UniversityAdministrationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UniversityAdministration.Models.Entities.Student> Student { get; set; }
+    }
+}
---
+++ Models/Entities/Enrollment.cs
@@ -0,0 +1,16 @@
+﻿namespace UniversityAdministration.Models.Entities
+{
+    public class Enrollment
+    {
+        public int EnrollmentId { get; set; }
+        public int StudentId { get; set; }
+        public int CourseId { get; set; }
+        public int Grade { get; set; }
+
+        // Navigation prop
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+
+
+    }
+}
